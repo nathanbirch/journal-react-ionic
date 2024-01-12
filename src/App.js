e005625c062d20845fe5d1e54ff8570ce6600bfd
corychoate <corychoate1999@gmail.com>
2024-01-12 14:26:05 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import { setupIonicReact } from '@ionic/react';
 import '@ionic/react/css/core.css';
 import logo from './logo.svg';
 import './App.css';
+import { IonButton, IonDatetime, IonContent, IonPage, setupIonicReact } from '@ionic/react';
 
 setupIonicReact();
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          This will be the home page or 'sign in' page.
-        </p>
+    <IonPage>
+      <IonContent>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <p>This will be the main or 'sign in' page</p>
+
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 }
 
