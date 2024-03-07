e08fb7bae46b83991816df9e74e96fa942943697
corychoate <corychoate1999@gmail.com>
2024-03-06 21:39:37 -0700

    added sidebar, pushed signout button to bottom of sidebar pop out. Edited color schemes.

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import { IonButton, IonContent, IonPage, IonIcon, setupIonicReact } from '@ionic/react';
 import { menu } from 'ionicons/icons';
 import '@ionic/react/css/core.css';
@@ -17,9 +17,38 @@ setupIonicReact();
 
 export default function Home() {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
+
+  function toggleSidebar() {
+    setIsSidebarOpen(!isSidebarOpen);
+  }
+    
+  useEffect(() => {
+    // Function to handle outside click
+    function handleOutsideClick(event) {
+      // Close the sidebar if the click is outside of the sidebar
+      if (!event.target.closest('.sidebar') && !event.target.closest('.menu_button')) {
+        setIsSidebarOpen(false);
+      }
+    }
+  
+
+    if (isSidebarOpen) {
+      document.addEventListener('click', handleOutsideClick);
+    }
+  
+    // Cleanup function to remove the event listener
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isSidebarOpen]);
+
+  
+  
   
 
   return (
@@ -30,19 +59,26 @@ export default function Home() {
         <div className="Home">
           <main className="Home-main">
           <h1>Welcome {user ? capitalizeFirstLetter(user.username) : ''}</h1>
-            <IonButton className='signout_button' onClick={signOut}>Sign out</IonButton>
 
             {/*for menu button in upper left corner*/}
-            <IonButton className="menu_button">
+            <IonButton className="menu_button" onClick={toggleSidebar}>
               <IonIcon icon={menu}/>
             </IonButton>
+
+            <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+            {/* Sidebar content goes here */}
+            <div className="sidebar-content">
+              <h2>Sidebar topics</h2>
+              <h2></h2>
+              <IonButton className='signout_button' onClick={signOut}>Sign out</IonButton>
+            </div>
+            </div>
 
             <Calendar
               onChange={date => setSelectedDate(date)}
               value={selectedDate}
               locale="en-US"
-              className='calendar'
-              />            
+              className='calendar'/>            
 
 
           </main>
